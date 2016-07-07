ec179217c406a1099c87dcc4fbefbdb2aec411f4
batmanpon <weeraponssuwanchatree@pi3.local>
2016-07-07 11:48:57 +0700

    update api

---
+++ TouchInsightAdmin/API_RestModel.swift
@@ -0,0 +1,17 @@
+//
+//  API_RestModel.swift
+//  TouchInsightAdmin
+//
+//  Created by weerapons suwanchatree on 7/7/2559 BE.
+//  Copyright © 2559 weerapons suwanchatree. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+//import CoreLocation
+class API_RestModel {
+    
+    func test() {
+    }
+    
+}
---
+++ TouchInsightAdmin/Restuarant.swift
@@ -0,0 +1,14 @@
+//
+//  Restuarant.swift
+//  TouchInsightAdmin
+//
+//  Created by weerapons suwanchatree on 7/7/2559 BE.
+//  Copyright © 2559 weerapons suwanchatree. All rights reserved.
+//
+
+import Foundation
+class Restuarant: NSObject {
+
+    var Name:String!
+    
+}
