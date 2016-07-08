7af5ecc97e81612a0bcae2164cb90e68996bce63
batmanpon <weerapon.3at@gmail.com>
2016-07-08 11:02:16 +0700

    แก้ link

--- TouchInsightAdmin/RestuarantListVC.swift
+++ TouchInsightAdmin/RestuarantListVC.swift
@@ -108,9 +108,9 @@ class RestuarantListVC: UIViewController , PagingMenuControllerDelegate
         
     }
     func initialInfoVC(){
-        infoViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ResInformation") as! ResInformationVC
-        LocationViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ResLocation") as! ResLocationVC
-        RoominfoViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ResMenuInfo") as! ResMenuInfoVC
+        infoViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ResInformationVC") as! ResInformationVC
+        LocationViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ResLocationVC") as! ResLocationVC
+        RoominfoViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ResMenuInfoVC") as! ResMenuInfoVC
         //        let viewControllers = [infoViewController, LocationViewController, RoominfoViewController]
         let viewControllers = [infoViewController,LocationViewController,RoominfoViewController]
         options.menuItemMode = .Underline(height: 3.0, color: UIColor.redColor(), horizontalPadding: 0, verticalPadding: 0)
