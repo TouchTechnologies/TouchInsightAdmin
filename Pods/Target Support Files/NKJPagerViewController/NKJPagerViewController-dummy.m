d8645b573bf9b49748e34a46e4bad83bbc72dadc
batmanpon <weerapon.3at@gmail.com>
2016-07-08 14:51:42 +0700

    all

---
+++ Pods/Target Support Files/NKJPagerViewController/NKJPagerViewController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NKJPagerViewController : NSObject
+@end
+@implementation PodsDummy_NKJPagerViewController
+@end
---
+++ Pods/Target Support Files/NKJPagerViewController/NKJPagerViewController-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "NKJPagerViewController.h"
+
+FOUNDATION_EXPORT double NKJPagerViewControllerVersionNumber;
+FOUNDATION_EXPORT const unsigned char NKJPagerViewControllerVersionString[];
+
